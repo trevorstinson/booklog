64fa078ac862da60a6f145a8c15556e02c1d421f
Trevor Stinson <trevor@trevorstinson.com>
2019-01-30 23:34:47 -0600

    Set up Gradle project

---
+++ src/main/java/com/trevorstinson/booklog/BooklogApplication.java
@@ -0,0 +1,14 @@
+package com.trevorstinson.booklog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooklogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooklogApplication.class, args);
+	}
+
+}
+
