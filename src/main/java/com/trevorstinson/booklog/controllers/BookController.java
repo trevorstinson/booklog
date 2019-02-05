845b0b301579318958c00af98f373a7fac72cac1
Trevor Stinson <trevor@trevorstinson.com>
2019-02-04 22:03:08 -0600

    Set up /book and index controller

--- src/main/java/com/trevorstinson/booklog/controllers/BookController.java
+++ src/main/java/com/trevorstinson/booklog/controllers/BookController.java
@@ -3,17 +3,19 @@ package com.trevorstinson.booklog.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
+@RequestMapping("book")
 public class BookController {
 
     @GetMapping(value = "")
     public String index(Model model) {
         model.addAttribute("bookTitle", "Dune");
         model.addAttribute("bookAuthor", "Frank Herbert");
-        return "index";
+        return "book/index";
     }
 
     @GetMapping(value = "goodbye")
