7cb7c52ea2cb6ff6d25c08a65cd9527ab272baa3
Trevor Stinson <trevor@trevorstinson.com>
2019-02-04 23:24:44 -0600

    Add Book model

--- src/main/java/com/trevorstinson/booklog/controllers/BookController.java
+++ src/main/java/com/trevorstinson/booklog/controllers/BookController.java
@@ -1,15 +1,20 @@
 package com.trevorstinson.booklog.controllers;
 
+import com.trevorstinson.booklog.models.Book;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+
 
 @Controller
 @RequestMapping("book")
 public class BookController {
+
+    static ArrayList<Book> books = new ArrayList<>();
 
     @GetMapping(value = "")
     public String index(Model model) {
---
+++ src/main/java/com/trevorstinson/booklog/models/Book.java
@@ -0,0 +1,32 @@
+package com.trevorstinson.booklog.models;
+
+public class Book {
+
+    private String title;
+    private String author;
+    private Integer pageCount;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Integer getPageCount() {
+        return pageCount;
+    }
+
+    public void setPageCount(Integer pageCount) {
+        this.pageCount = pageCount;
+    }
+}
