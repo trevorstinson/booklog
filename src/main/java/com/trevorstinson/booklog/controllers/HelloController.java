7c142be1e6051d9eee30490dd4f602b6bdc87322
Trevor Stinson <trevor@trevorstinson.com>
2019-02-04 20:59:37 -0600

    Set up Vue and Thymeleaf

--- src/main/java/com/trevorstinson/booklog/controllers/HelloController.java
+++ src/main/java/com/trevorstinson/booklog/controllers/HelloController.java
@@ -1,23 +1,24 @@
 package com.trevorstinson.booklog.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
 public class HelloController {
 
-    @RequestMapping(value = "")
-    @ResponseBody
-    public String index() {
-        return "Hello Reader";
+    @GetMapping(value = "")
+    public String index(Model model) {
+        model.addAttribute("bookName", "The Name of the Wind");
+        return "index";
     }
 
-    @RequestMapping(value = "goodbye")
+    @GetMapping(value = "goodbye")
     @ResponseBody
     public String goodbye() {
         return "Goodbye Reader";
     }
-    
+
 }
