d6bd93e62a4de66cd3aa12c2da6d879124bc4b22
Trevor Stinson <trevor@trevorstinson.com>
2019-03-03 16:40:50 -0600

    Begin implementing user roles

---
+++ src/main/java/com/trevorstinson/booklog/models/User.java
@@ -0,0 +1,64 @@
+package com.trevorstinson.booklog.models;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.Collection;
+
+@Entity
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    @Size(min = 8, max = 20)
+    private String username;
+
+    @Column(length = 100)
+    private String password;
+
+    @Column(nullable = false)
+    private boolean enabled;
+
+
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
