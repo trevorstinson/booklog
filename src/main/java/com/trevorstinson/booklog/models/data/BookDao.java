767778f6f44f95365778e87a511b793ac8a59ec8
Trevor Stinson <trevor@trevorstinson.com>
2019-03-15 00:56:28 -0500

    Add findBy methods to BookDao for all Book fields

--- src/main/java/com/trevorstinson/booklog/models/data/BookDao.java
+++ src/main/java/com/trevorstinson/booklog/models/data/BookDao.java
@@ -2,11 +2,29 @@ package com.trevorstinson.booklog.models.data;
 
 
 import com.trevorstinson.booklog.models.Book;
+import com.trevorstinson.booklog.models.BookStatus;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+
 @Repository
 @Transactional
 public interface BookDao extends CrudRepository<Book, Integer> {
+
+    Iterable<Book> findByTitle(String title);
+
+    Iterable<Book> findByAuthor(String author);
+
+    Iterable<Book> findByPageCount(Integer pageCount);
+
+    Iterable<Book> findByStatus(BookStatus status);
+
+    Iterable<Book> findByIsbn(String isbn);
+
+    Iterable<Book> findByDateStarted(LocalDate dateStarted);
+
+    Iterable<Book> findByDateFinished(LocalDate dateFinished);
+
 }
