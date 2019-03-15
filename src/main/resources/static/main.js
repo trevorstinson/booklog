62607a5fcc3ad9f4caaee7083572ebee8d26bbe3
Trevor Stinson <trevor@trevorstinson.com>
2019-03-15 16:12:28 -0500

    Set up call to book data API

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -2,10 +2,10 @@ let app = new Vue({
     el: "#root",
 
     data: {
-        books: {},
         bookQuery: "",
         searchResults: [],
         showAddBookForm: true,
+        bookList: []
     },
 
     methods: {
@@ -13,6 +13,19 @@ let app = new Vue({
             axios
                 .get("http://openlibrary.org/search.json?jscmd=data&title=" + this.bookQuery)
                 .then(response => (this.searchResults = response.data.docs));
+
+                for (var i = 0; i < this.searchResults.length; i++) {
+
+//                    var currentBook = {};
+
+                    if (this.searchResults[i].isbn) {
+                        axios
+                            .get("http://openlibrary.org/api/books?bibkeys=ISBN:" + this.searchResults[i].isbn[0] + "&format=json")
+                            .then(response => (currentBook = response.data.docs));
+
+                            this.bookList.push(currentBook);
+                    }
+                }
 
             this.showAddBookForm = false;
 
